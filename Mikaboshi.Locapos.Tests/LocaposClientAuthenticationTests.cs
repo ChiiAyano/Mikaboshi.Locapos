d1974c8f76aba4eaaea2d37b60b2cddd05aad331
綾野ちい (Chii Ayano) <git@aynv.jp>
2026-05-02 16:41:32 +0900

    test(locapos): 認証・トークン検証の単体テストを追加
    Mikaboshi.Locapos.Tests プロジェクトを新規作成し、LocaposClient の認証URI生成・トークン解析・トークン検証(CheckToken)のテストを xUnit で実装しました。Mikaboshi.Locapos への参照を追加し、.NET 8.0 環境で動作します。

---
+++ Mikaboshi.Locapos.Tests/LocaposClientAuthenticationTests.cs
@@ -0,0 +1,69 @@
+﻿using Mikaboshi.Locapos;
+
+namespace Mikaboshi.Locapos.Tests;
+
+public class LocaposClientAuthenticationTests
+{
+    [Fact]
+    public void GetAuthenticationUri_WhenNormalMode_UsesLocaposDomain()
+    {
+        var client = new LocaposClient(isBeta: false);
+
+        var uri = client.GetAuthenticationUri("api-key", new Uri("mikaboshi://auth"));
+
+        Assert.StartsWith("https://locapos.com/oauth/authorize", uri.ToString());
+    }
+
+    [Fact]
+    public void GetAuthenticationUri_WhenBetaMode_UsesBetaLocaposDomain()
+    {
+        var client = new LocaposClient(isBeta: true);
+
+        var uri = client.GetAuthenticationUri("api-key", new Uri("mikaboshi://auth"));
+
+        Assert.StartsWith("https://beta.locapos.com/oauth/authorize", uri.ToString());
+    }
+
+    [Fact]
+    public void GetAuthenticationUri_ContainsClientIdAndRedirectUri()
+    {
+        var client = new LocaposClient();
+
+        var uri = client.GetAuthenticationUri("key+space value", new Uri("mikaboshi://auth/callback?x=1&y=2"));
+
+        Assert.Contains("client_id=", uri.ToString());
+        Assert.Contains("redirect_uri=", uri.ToString());
+    }
+
+    [Fact]
+    public void ParseAuthenticationResponse_SetsClientToken()
+    {
+        var client = new LocaposClient();
+
+        var token = client.ParseAuthenticationResponse("mikaboshi://auth#access_token=abc123&token_type=bearer");
+
+        Assert.Equal("abc123", token.Token);
+        Assert.NotNull(client.ClientToken);
+        Assert.Equal("abc123", client.ClientToken!.Token);
+    }
+
+    [Fact]
+    public void ParseAuthenticationResponse_WithUrlPrefix_ParsesFragmentToken()
+    {
+        var client = new LocaposClient();
+
+        var token = client.ParseAuthenticationResponse("https://locapos.com/callback#access_token=xyz&scope=read");
+
+        Assert.Equal("xyz", token.Token);
+    }
+
+    [Fact]
+    public void ParseAuthenticationResponse_WithoutHash_StillParsesToken()
+    {
+        var client = new LocaposClient();
+
+        var token = client.ParseAuthenticationResponse("access_token=abc");
+
+        Assert.Equal("abc", token.Token);
+    }
+}
---
+++ Mikaboshi.Locapos.Tests/LocaposClientTokenTests.cs
@@ -0,0 +1,54 @@
+﻿using Mikaboshi.Locapos;
+
+namespace Mikaboshi.Locapos.Tests;
+
+public class LocaposClientTokenTests
+{
+    [Fact]
+    public void CheckToken_WhenClientTokenIsNull_ThrowsTokenNotFoundException()
+    {
+        var client = new LocaposClient();
+
+        var method = typeof(LocaposClient).GetMethod("CheckToken", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
+        Assert.NotNull(method);
+
+        var ex = Assert.Throws<System.Reflection.TargetInvocationException>(() => method.Invoke(client, null));
+        Assert.IsType<TokenNotFoundException>(ex.InnerException);
+    }
+
+    [Fact]
+    public void CheckToken_WhenClientTokenIsEmpty_ThrowsTokenNotFoundException()
+    {
+        var client = new LocaposClient { ClientToken = new ClientToken { Token = "" } };
+
+        var method = typeof(LocaposClient).GetMethod("CheckToken", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
+        Assert.NotNull(method);
+
+        var ex = Assert.Throws<System.Reflection.TargetInvocationException>(() => method.Invoke(client, null));
+        Assert.IsType<TokenNotFoundException>(ex.InnerException);
+    }
+
+    [Fact]
+    public void CheckToken_WhenClientTokenIsWhitespace_ThrowsTokenNotFoundException()
+    {
+        var client = new LocaposClient { ClientToken = new ClientToken { Token = "   " } };
+
+        var method = typeof(LocaposClient).GetMethod("CheckToken", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
+        Assert.NotNull(method);
+
+        var ex = Assert.Throws<System.Reflection.TargetInvocationException>(() => method.Invoke(client, null));
+        Assert.IsType<TokenNotFoundException>(ex.InnerException);
+    }
+
+    [Fact]
+    public void CheckToken_WhenClientTokenIsValid_DoesNotThrow()
+    {
+        var client = new LocaposClient { ClientToken = new ClientToken { Token = "valid-token" } };
+
+        var method = typeof(LocaposClient).GetMethod("CheckToken", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
+        Assert.NotNull(method);
+
+        var exception = Record.Exception(() => method.Invoke(client, null));
+        Assert.Null(exception);
+    }
+}
